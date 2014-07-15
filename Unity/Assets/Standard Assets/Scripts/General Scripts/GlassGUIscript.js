daabcc56a181a46e50e7d28ea660db85f5daa16e
JELGT2011 <jelgt2011@gmail.com>
2014-07-15 17:08:21 -0400

    Created Google Glass GUI object and basic template
    Google Glass GUI GameObject and GUI script, as well as custom GUIStyle
    for formatting.

---
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/GlassGUIscript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var customGUIStyle:GUIStyle;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+	GUI.BeginGroup(Rect(Screen.width-210, 10, 200, 200));
+	
+	GUI.Box(Rect(0, 0, 200, 200), "");
+	GUI.TextArea(Rect(0, 0, 200, 50), "Google Glass", customGUIStyle);
+	GUI.TextArea(Rect(0, 20, 200, 50), "Wind Speed", customGUIStyle);
+	GUI.TextArea(Rect(0, 40, 100, 50), "x: 10mph", customGUIStyle);
+	GUI.TextArea(Rect(100, 40, 100, 50), "y: 12mph", customGUIStyle);
+	GUI.TextArea(Rect(0, 60, 200, 50), "I'll format it later", customGUIStyle);
+    GUI.EndGroup();
+}
