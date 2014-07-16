0c62e5ff1b033b43a6b503fcf43da9a7be228514
drewgolas <drewgolas@gmail.com>
2014-07-16 14:26:01 -0400

    public variables, reset
    Resets football after kick, made various variables public

--- Unity/Assets/Standard Assets/Scripts/Kick.cs
+++ Unity/Assets/Standard Assets/Scripts/Kick.cs
@@ -3,21 +3,29 @@ using System.Collections;
 
 public class Kick : MonoBehaviour {
 
+	public float vert;
+	public float strength;
+
+	private Vector3 startPos;
 	// Use this for initialization
 	void Start () {
-	
+		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(transform.position.y<-5){
+			transform.position = startPos;
+			rigidbody.velocity = Vector3.zero;
+		}
 	}
 
 	void OnTriggerEnter(Collider col){
 		if(col.tag=="Player"){
 			Vector3 direct = col.transform.forward;
-			direct.y = 1f;
-			rigidbody.AddForce(direct*700);
+			direct = direct * strength;
+			direct.y = vert*strength;
+			rigidbody.AddForce(direct);
 			print (direct);
 		}
 
