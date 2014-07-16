70406f22e9f5b51297bf6777da226339e41d8ea8
JELGT2011 <jelgt2011@gmail.com>
2014-07-16 15:46:13 -0400

    created wind object and script

---
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/Wind_script.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var windSound:AudioSource;
+
+function Start () {
+  transform.position = Vector3(Random.value*10 , 0, Random.value*10);
+  windSound.Play();
+}
+
+function Update () {
+
+}
