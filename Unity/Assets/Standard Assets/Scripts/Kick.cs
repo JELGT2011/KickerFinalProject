b95f0835a1eb44d4b2ded5e5a11e790af28cdb0e
drewgolas <drewgolas@gmail.com>
2014-07-16 13:24:42 -0400

    Football & Kick script
    football prefab and the kicking script original

---
+++ Unity/Assets/Standard Assets/Scripts/Kick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag=="Player"){
+			Vector3 direct = col.transform.forward;
+			direct.y = 1f;
+			rigidbody.AddForce(direct*700);
+			print (direct);
+		}
+
+	}
+}
