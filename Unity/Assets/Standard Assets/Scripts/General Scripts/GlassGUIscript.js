3249adbe44a8eee05edb914d096292842ef55816
drewgolas <drewgolas@gmail.com>
2014-07-16 15:22:46 -0400

    pictures!
    wohoo!

--- Unity/Assets/Standard Assets/Scripts/General Scripts/GlassGUIscript.js
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/GlassGUIscript.js
@@ -3,13 +3,25 @@
 var customGUIStyle:GUIStyle;
 
 	var images : Texture2D[];
+	private var select: float;
 
 function Start () {
-
+	select = 0;
 }
 
 function Update () {
-
+	
+	if(Input.GetKeyDown("j"))
+		select+=1;
+		
+	if(Input.GetKeyDown("k"))
+		select-=1;
+	if(select>images.Length-1){
+		select=0;
+	}
+	if(select<0)
+		select = images.Length-1;
+	
 }
 
 function OnGUI() {
@@ -20,6 +32,7 @@ function OnGUI() {
 	GUI.TextArea(Rect(0, 20, 200, 50), "Wind Speed", customGUIStyle);
 	GUI.TextArea(Rect(0, 40, 100, 50), "x: 10mph", customGUIStyle);
 	GUI.TextArea(Rect(100, 40, 100, 50), "y: 12mph", customGUIStyle);
-	GUI.TextArea(Rect(0, 60, 200, 50), "I'll format it later", customGUIStyle);
+	//GUI.TextArea(Rect(0, 60, 200, 50), "I'll format it later", customGUIStyle);
+	GUI.DrawTexture(Rect(25,100,100,50),images[select]);
     GUI.EndGroup();
 }
