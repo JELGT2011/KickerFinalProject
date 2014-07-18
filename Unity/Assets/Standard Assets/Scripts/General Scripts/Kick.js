fc516d7505d9c6b5122bff1fda3fed78b8ac3f85
Kevin Guebert <kguebert93@gmail.com>
2014-07-18 12:17:46 -0400

    kick.js
    convert cs to js

---
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/Kick.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var vert : float;
+var strength : float;
+var startPos : Vector3;
+var direct : Vector3;
+
+function Start () {
+	startPos = transform.position;
+}
+
+function Update () {
+	if(transform.position.y<-5){
+		transform.position = startPos;
+		rigidbody.velocity = Vector3.zero;
+	}
+}
+
+function OnTriggerEnter( col : Collider ){
+	if(col.tag=="Player"){
+		direct = col.transform.forward;
+		direct = direct * strength;
+		direct.y = vert*strength;
+		rigidbody.AddForce(direct);
+	}
+}
