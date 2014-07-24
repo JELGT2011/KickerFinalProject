0e0b0c61ed340f8110b7e7dfb5ab972452f06938
drewgolas <drewgolas@gmail.com>
2014-07-24 19:29:08 -0400

    kick correction sounds are a go

--- Unity/Assets/Standard Assets/Scripts/General Scripts/Kick.js
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/Kick.js
@@ -49,23 +49,25 @@ function Update () {
 	}
 }
 
-function OnCollisionEnter(collision : Collision) {
-	switch (kickType) {
-		case 1:
-			leftKick();
-			break;
-		case 2:
-			rightKick();
-			break;
-		case 3:
-			forKick();
-			break;
-		case 4:
-			backKick();
-			break;
-		case 5:
-			goodKick();
-			break;
+function OnTriggerEnter(collision : Collider) {
+	if(collision.tag =="Player"){
+		switch (kickType) {
+			case 1:
+				leftKick();
+				break;
+			case 2:
+				rightKick();
+				break;
+			case 3:
+				forKick();
+				break;
+			case 4:
+				backKick();
+				break;
+			case 5:
+				goodKick();
+				break;
+		}
 	}
 		
 }
@@ -73,36 +75,37 @@ function OnCollisionEnter(collision : Collision) {
 function leftKick(){
 	direct = Vector3(-.7,1.2,.3);
 	rigidbody.AddForce(direct*strength);
-	correct.sideSound(.3);
+	correct.sideSound(.3,0);
 }
 
 function rightKick(){
 	direct = Vector3(-.6,.8,-.6);
 	rigidbody.AddForce(direct*strength);
-	correct.sideSound(-.6);
+	correct.sideSound(-.6,-1);
 }
 
 function forKick(){
 	direct = Vector3(-.4,1.7,0);
 	rigidbody.AddForce(direct*strength);
-	correct.footForward(1.7);
+	correct.footForward(1);
 }
 
 function backKick(){
 	direct = Vector3(-1.0,.6,0);
 	rigidbody.AddForce(direct*strength);
-	correct.footForward(.6);
+	correct.footForward(-1);
 }
 function goodKick(){
 	direct = Vector3(-1.0,1.2,0);
 	rigidbody.AddForce(direct*strength);
+	correct.goodKick();
 }
 
 function reset(){
 	transform.position = startPos;
 	rigidbody.velocity = Vector3.zero;
 	transform.rotation = startRot;
-	gone = false;
+	gone = false;	
 }
 	
 
--- Unity/Assets/Standard Assets/Scripts/General Scripts/soundCor.js
+++ Unity/Assets/Standard Assets/Scripts/General Scripts/soundCor.js
@@ -8,23 +8,30 @@ private var startPos: Vector3;
 private var dopplerS : Vector3;
 private var dopplerE : Vector3;
 private var percent : float = .1;
+private var secondary : float = 0;
 
 function Start () {
-	startPos = transform.position;
+	startPos = transform.localPosition;
 }
 
 function Update () {
 	if(forwarding){
-		transform.position = Vector3.Lerp(dopplerS,dopplerE,percent);
+		transform.localPosition = Vector3.Lerp(dopplerS,dopplerE,percent);
 		percent += Time.deltaTime;
 	}
 	if(percent>=1)
 		forwarding=false;
+	if(secondary!=0){
+		if(!audio.isPlaying){
+			footForward(secondary);
+			secondary =0;
+		}
+	}
 
 }
 
-function sideSound(magn : float){
-	transform.position = startPos;
+function sideSound(magn : float, placement : float){
+	transform.localPosition = startPos;
 	if(magn<0){
 		transform.Translate(Vector3(0,0,-1));
 	}
@@ -33,22 +40,26 @@ function sideSound(magn : float){
 	}
 	leftRight.volume = Mathf.Abs(magn);
 	leftRight.Play();
+	secondary = placement;
 }
 
 function footForward(magn : float){
 	percent =0;
-	transform.position = startPos;
-	if(magn<1.2){
-		dopplerS = startPos + Vector3(5,0,0);
+	transform.localPosition = startPos;
+	if(magn>0){
+		dopplerS = startPos + Vector3(0,0,5);
 		dopplerE = startPos;
-		transform.position=dopplerS;
+		transform.localPosition=dopplerS;
 	}
-	else if(magn>1.2){
-		dopplerE = startPos + Vector3(-5,0,0);
+	else if(magn<0){
+		dopplerE = startPos + Vector3(0,0,-5);
 		dopplerS = startPos;
-		transform.position = dopplerS;
+		transform.localPosition = dopplerS;
 	}
 	forBack.volume=1;
 	forBack.Play();	
 	forwarding=true;
+}
+function goodKick(){
+	rightOn.Play();
 }
